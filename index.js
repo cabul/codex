d6dfcd48d4dd7cee9dc691ec3d12b24eb424f69b
Calvin Bulla <calvin.bulla@gmail.com>
2015-06-11 22:47:32 +0100

    Added basic files

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('Hello World')
+const BrowserWindow = require('browser-window');
+const crashReporter = require('crash-reporter');
+const app = require('app');
+const path = require('path');
+
+var mainWindow = null;
+
+crashReporter.start();
+
+app.on('windows-all-closed', function() {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({
+    title: 'Codex',
+    width: 800,
+    height: 600
+  });
+  mainWindow.loadUrl('file://' + path.join(__dirname, '/index.html'));
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
